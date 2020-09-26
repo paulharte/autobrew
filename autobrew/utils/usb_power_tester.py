50a992e92bef13f65c67a0e052752c4dff98ee16
paulharte <hartepaul@gmail.com>
2020-09-26 20:12:13 +0100

    usb test fix

--- autobrew/utils/usb_power_tester.py
+++ autobrew/utils/usb_power_tester.py
@@ -3,8 +3,13 @@ import sys
 
 
 if __name__ == "__main__":
-    on_off_arg = sys.argv[0]
-    if on_off_arg == 0:
+    on_off_arg = str(sys.argv[1])
+    if on_off_arg == '0':
         switch_power(False)
-    elif on_off_arg == 1:
+        print("Power switched off")
+    elif on_off_arg == '1':
         switch_power(True)
+        print("power swtiched on")
+    else:
+        print("invalid args" + str(sys.argv))
+
