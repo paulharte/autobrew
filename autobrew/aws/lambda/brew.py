141804455e635ac4109aefc6416c7b95e101b377
Paul Harte <hartepaul@gmail.com>
2021-01-17 17:15:38 +0000

    Much better AWS lambda functions

---
+++ autobrew/aws/lambda/brew.py
@@ -0,0 +1,41 @@
+import json
+
+
+
+
+class Serializable():
+
+    def to_dict(self):
+        return self.__dict__
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.toDict(), sort_keys=True, indent=4)
+
+    @classmethod
+    def from_json(cls, json_string):
+        attributes = json.loads(json_string)
+        if not isinstance(attributes, dict):
+            raise ValueError
+        return cls.from_dict(attributes)
+
+    @classmethod
+    def from_dict(cls, attributes):
+        obj = cls()
+        obj.__dict__ = attributes
+        return obj
+
+
+class Brew(Serializable):
+    id: str
+    name: str
+    active = False
+
+    def __init__(self):
+        pass
+
+    #TODO: add validation here
+
+
+
+
+
--- autobrew/aws/lambda/handler.py
+++ autobrew/aws/lambda/handler.py
@@ -1,32 +1,49 @@
 
 import json
-import boto3
+from .brew import Brew
+from .brewService import BrewService
 
-BREWS_DYNAMO_TABLE = 'autobrew_brew'
-DYNAMO_DB = 'dynamodb'
-
-
-def create_brew(event, context):
-    dynamodb = boto3.resource(DYNAMO_DB)
-    table = dynamodb.Table(BREWS_DYNAMO_TABLE)
-
-    brew = {'id': event['name'],
-            'active': event.get('active', False),
-            'name': event['name']}
-    response = table.put_item(Item=brew)
-
+def create_brew(event: dict, context):
+    brew = Brew.from_json(event['body'])
+    response = BrewService().create(brew)
     return {
         'statusCode': 200,
         'body': json.dumps(response)
     }
 
 
-def get_brews(event, context):
-    dynamodb = boto3.resource(DYNAMO_DB)
-    table = dynamodb.Table(BREWS_DYNAMO_TABLE)
-    brews = table.scan()
+def get_brews(event: dict, context):
+    brews = BrewService().getAll()
     return {
         'statusCode': 200,
         'body': json.dumps(brews)
+    }
+
+def get_brew(event: dict , context):
+    id_to_get = event['pathParameters']['id']
+    brew = BrewService().get(id_to_get)
+
+    if not brew:
+        return {'statusCode': 404, 'body': json.dumps({'error': 'Brew does not exist'})}
+
+    return {
+        'statusCode': 200,
+        'body': brew.to_json()
+    }
+
+def update_brew(event, context):
+    brew = Brew.from_json(event['body'])
+    response = BrewService().put(brew)
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response)
+    }
+
+def delete_brew(event, context):
+    id_to_delete = event['pathParameters']['id']
+    response = BrewService().delete(id_to_delete)
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response)
     }
 
