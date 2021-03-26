203c96caf99966c7cc00caeef99d2a39f0bcdf14
Paul Harte <hartepaul@gmail.com>
2021-03-26 19:33:46 +0000

    serverless fixes and more

--- ui/src/app/brew/measurement/series/series.component.ts
+++ ui/src/app/brew/measurement/series/series.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChange } from '@angular/core';
 import { ChartType } from 'angular-google-charts';
 import { MeasurementSeries } from '../../brew.models';
 
@@ -11,12 +11,32 @@ export class SeriesComponent implements OnInit {
 
   @Input() series!: MeasurementSeries;
 
+  public columns = ['Time', 'Measurements'];
   public chartType = ChartType.Line;
-  public chartData = [];
+  public chartData: any[][] = [];
+  public options = {
+    width: 900,
+    height: 400};
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChange) {
+    if (this.series) {
+      const out = [];
+      for (const measure of this.series.measurements) {
+        out.push([measure.time, measure.measurement_amt])
+      }
+      this.chartData = out;
+
+      this.updateMesurementName(this.series.getDisplayName())
+    }
+  }
+
+  updateMesurementName(name: string) {
+    this.columns = [this.columns[0], name]
   }
 
 }
