4a0e03f0870e07c3ba91b2b242be17024845a6c1
Paul Harte <hartepaul@gmail.com>
2021-04-01 16:50:34 +0100

    Scripting and black

--- runner.sh
+++ runner.sh
@@ -1,13 +1,14 @@
 python3 -m venv ./.venv
 source .venv/bin/activate
-pip install -r requirements.txt
+pip install -r requirements.txt > null
 echo "Starting Autobrew"
 if [ $1 == "uat" ]
 then
         nohup python3 -u ./main.py "uat" > brew.log &
         echo "running UAT"
 else
-        nohup python3 -u ./main.py > brew.log &
+        nohup python3 -u ./main.py "prod" > brew.log &
         echo "running prod"
 fi
 echo "Autobrew started"
+tail -f brew.log
